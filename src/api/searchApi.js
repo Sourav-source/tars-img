dfdbf7cbfa8fd2bff1c7059aa60f3e962a7b1bd7
Sourav-source <st.sourav.xaverian@gmail.com>
2023-10-22 16:33:10 +0530

    set timeout to api call

--- src/api/searchApi.js
+++ src/api/searchApi.js
@@ -2,10 +2,20 @@ import axios from "axios";
 const accessKey = "hVYRz-TuHNY5WVuH5HbmL09aDtUBPlaSX_FFAuMPJAc";
 
 export const searchImg = async (query) => {
-  const response = await axios.get(
-    `https://api.unsplash.com/search/photos?query=${query}&per_page=10&client_id=${accessKey}`
-  );
-  if (response.status === 200) {
-    return { status: response.status, data: response.data };
-  } else throw new Error(`HTTP error: ${response.status}`);
+  return new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      try {
+        const response = await axios.get(
+          `https://api.unsplash.com/search/photos?query=${query}&per_page=10&client_id=${accessKey}`
+        );
+        if (response.status === 200) {
+          resolve({ status: response.status, data: response.data });
+        } else {
+          reject(new Error(`HTTP error: ${response.status}`));
+        }
+      } catch (error) {
+        reject(error);
+      }
+    }, 3000);
+  });
 };
