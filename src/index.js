141ea9dd26736a870052408f88d082960c9b83ae
Sourav-source <st.sourav.xaverian@gmail.com>
2023-10-22 16:21:55 +0530

    redux persist state being managed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
 import { Provider } from "react-redux";
 import store from "./redux/store";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const myPersistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={myPersistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import imageReducer from "./searchSlice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const myCombinedReducer = combineReducers({
+  image: imageReducer,
+});
+
+const localStorageReducer = persistReducer(persistConfig, myCombinedReducer);
 
 const store = configureStore({
-  reducer: {
-    image: imageReducer,
-  },
+  reducer: localStorageReducer,
 });
 
 export default store;
